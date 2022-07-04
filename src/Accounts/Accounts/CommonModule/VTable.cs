00a0a21587ce845a6e4ce2846a2ff570de060ddc
VeryEarly <yabhu@microsoft.com>
2022-07-04 15:22:49 +0800

    compute

--- src/Accounts/Accounts/CommonModule/VTable.cs
+++ src/Accounts/Accounts/CommonModule/VTable.cs
@@ -22,9 +22,11 @@ namespace Microsoft.Azure.Commands.Common
     using EventListenerDelegate = Func<string, CancellationToken, Func<EventArgs>, Func<string, CancellationToken, Func<EventArgs>, Task>, System.Management.Automation.InvocationInfo, string, string, string, System.Exception, Task>;
     using EventListenerWithoutTelemetryDelegate = Func<string, CancellationToken, Func<EventArgs>, Func<string, CancellationToken, Func<EventArgs>, Task>, Task>;
     using GetParameterDelegate = Func<string, string, System.Management.Automation.InvocationInfo, string, string, object>;
+    using TelemetryDelegate = Action<string, System.Management.Automation.InvocationInfo, string, System.Management.Automation.PSCmdlet>;
+    using GetTelemetryIdDelegate = Func<string>;
+    using NewProxyRequestPipelineDelegate = global::System.Action<global::System.Action<global::System.Func<global::System.Net.Http.HttpRequestMessage, global::System.Threading.CancellationToken, global::System.Action, global::System.Func<string, global::System.Threading.CancellationToken, global::System.Func<global::System.EventArgs>, global::System.Threading.Tasks.Task>, global::System.Func<global::System.Net.Http.HttpRequestMessage, global::System.Threading.CancellationToken, global::System.Action, global::System.Func<string, global::System.Threading.CancellationToken, global::System.Func<global::System.EventArgs>, global::System.Threading.Tasks.Task>, global::System.Threading.Tasks.Task<global::System.Net.Http.HttpResponseMessage>>, global::System.Threading.Tasks.Task<global::System.Net.Http.HttpResponseMessage>>>, global::System.Action<global::System.Func<global::System.Net.Http.HttpRequestMessage, global::System.Threading.CancellationToken, global::System.Action, global::System.Func<string, global::System.Threading.CancellationToken, global::System.Func<global::System.EventArgs>, global::System.Threading.Tasks.Task>, global::System.Func<global::System.Net.Http.HttpRequestMessage, global::System.Threading.CancellationToken, global::System.Action, global::System.Func<string, global::System.Threading.CancellationToken, global::System.Func<global::System.EventArgs>, global::System.Threading.Tasks.Task>, global::System.Threading.Tasks.Task<global::System.Net.Http.HttpResponseMessage>>, global::System.Threading.Tasks.Task<global::System.Net.Http.HttpResponseMessage>>>>;
     using ModuleLoadPipelineDelegate = Action<string, string, Action<Func<HttpRequestMessage, CancellationToken, Action, Func<string, CancellationToken, Func<EventArgs>, Task>, Func<HttpRequestMessage, CancellationToken, Action, Func<string, CancellationToken, Func<EventArgs>, Task>, Task<HttpResponseMessage>>, Task<HttpResponseMessage>>>, Action<Func<HttpRequestMessage, CancellationToken, Action, Func<string, CancellationToken, Func<EventArgs>, Task>, Func<HttpRequestMessage, CancellationToken, Action, Func<string, CancellationToken, Func<EventArgs>, Task>, Task<HttpResponseMessage>>, Task<HttpResponseMessage>>>>;
     using NewRequestPipelineDelegate = Action<System.Management.Automation.InvocationInfo, string, string, Action<Func<HttpRequestMessage, CancellationToken, Action, Func<string, CancellationToken, Func<EventArgs>, Task>, Func<HttpRequestMessage, CancellationToken, Action, Func<string, CancellationToken, Func<EventArgs>, Task>, Task<HttpResponseMessage>>, Task<HttpResponseMessage>>>, Action<Func<HttpRequestMessage, CancellationToken, Action, Func<string, CancellationToken, Func<EventArgs>, Task>, Func<HttpRequestMessage, CancellationToken, Action, Func<string, CancellationToken, Func<EventArgs>, Task>, Task<HttpResponseMessage>>, Task<HttpResponseMessage>>>>;
-    using NewProxyRequestPipelineDelegate = global::System.Action<global::System.Action<global::System.Func<global::System.Net.Http.HttpRequestMessage, global::System.Threading.CancellationToken, global::System.Action, global::System.Func<string, global::System.Threading.CancellationToken, global::System.Func<global::System.EventArgs>, global::System.Threading.Tasks.Task>, global::System.Func<global::System.Net.Http.HttpRequestMessage, global::System.Threading.CancellationToken, global::System.Action, global::System.Func<string, global::System.Threading.CancellationToken, global::System.Func<global::System.EventArgs>, global::System.Threading.Tasks.Task>, global::System.Threading.Tasks.Task<global::System.Net.Http.HttpResponseMessage>>, global::System.Threading.Tasks.Task<global::System.Net.Http.HttpResponseMessage>>>, global::System.Action<global::System.Func<global::System.Net.Http.HttpRequestMessage, global::System.Threading.CancellationToken, global::System.Action, global::System.Func<string, global::System.Threading.CancellationToken, global::System.Func<global::System.EventArgs>, global::System.Threading.Tasks.Task>, global::System.Func<global::System.Net.Http.HttpRequestMessage, global::System.Threading.CancellationToken, global::System.Action, global::System.Func<string, global::System.Threading.CancellationToken, global::System.Func<global::System.EventArgs>, global::System.Threading.Tasks.Task>, global::System.Threading.Tasks.Task<global::System.Net.Http.HttpResponseMessage>>, global::System.Threading.Tasks.Task<global::System.Net.Http.HttpResponseMessage>>>>;
     using ArgumentCompleterDelegate = Func<string, System.Management.Automation.InvocationInfo, string, string[], string[], string[]>;
     using AuthorizeRequestDelegate = global::System.Action<System.Management.Automation.InvocationInfo,
                                         string,
@@ -42,26 +44,37 @@ namespace Microsoft.Azure.Commands.Common
         /// <summary>
         /// The cmdlet will call this when it is trying to fill in a parameter value that it needs
         /// </summary>
-        /// <param name="resourceId"><c>string</c>containing the expected resource id (ie, ARM).</param>
-        /// <param name="moduleName"><c>string</c>containing the name of the module being loaded.</param>
-        /// <param name="invocationInfo">The <see cref="System.Management.Automation.InvocationInfo" /> from the cmdlet</param>
-        /// <param name="correlationId">The <see cref="string" /> containing the correlation id for the cmdlet</param>
-        /// <param name="name">The <see cref="string" /> parameter name being asked for</param>
-        /// <example>public object GetParameterValue(string resourceId, string moduleName, System.Management.Automation.InvocationInfo invocationInfo, string name)</example>
+        /// <example>
+        /// <code>
+        /// public object GetParameterValue(string resourceId, string moduleName, System.Management.Automation.InvocationInfo invocationInfo, string name)
+        /// </code>
+        /// <para>resourceId: <c>string</c>containing the expected resource id (ie, ARM).</para>
+        /// <para>moduleName: <c>string</c>containing the name of the module being loaded.</para>
+        /// <para>invocationInfo: The <see cref="System.Management.Automation.InvocationInfo" /> from the cmdlet</para>
+        /// <para>correlationId: The <see cref="string" /> containing the correlation id for the cmdlet</para>
+        /// <para>name: The <see cref="string" /> parameter name being asked for</para>
+        /// </example>
         public GetParameterDelegate GetParameterValue;
+
+        public GetTelemetryIdDelegate GetTelemetryId;
+
+        public TelemetryDelegate Telemetry;
+
 
         /// <summary>
         /// The cmdlet will call this for every event during the pipeline. 
         /// </summary>
-        /// <param name="id">a <c>string</c> containing the name of the event being raised (well-known events are in <see cref="Microsoft.Azure.Commands.Common.Events"/></param>
-        /// <param name="cancellationToken">a <c>CancellationToken</c> indicating if this request is being cancelled.</param>
-        /// <param name="getEventData">a delegate to call to get the event data for this event</param>
-        /// <param name="signal">a delegate to signal an event from the handler to the cmdlet.</param>
-        /// <param name="invocationInfo">The <see cref="System.Management.Automation.InvocationInfo" /> from the cmdlet</param>
-        /// <param name="parameterSetName">The <see cref="string" /> containing the name of the parameter set for this invocation (if available></param>
-        /// <param name="correlationId">The <see cref="string" /> containing the correlation id for the cmdlet (if available)</param>
-        /// <param name="processRecordId">The <see cref="string" /> containing the correlation id for the individual process record. (if available)</param>
-        /// <param name="exception">The <see cref="System.Exception" /> that is being thrown (if available)</param>
+        /// <example>
+        /// <para>id: a <c>string</c> containing the name of the event being raised (well-known events are in <see cref="Microsoft.Azure.Commands.Common.Events"/></para>
+        /// <para>cancellationToken: a <c>CancellationToken</c> indicating if this request is being cancelled.</para>
+        /// <para>getEventData: a delegate to call to get the event data for this event</para>
+        /// <para>signal: a delegate to signal an event from the handler to the cmdlet.</para>
+        /// <para>invocationInfo: The <see cref="System.Management.Automation.InvocationInfo" /> from the cmdlet</para>
+        /// <para>parameterSetName: The <see cref="string" /> containing the name of the parameter set for this invocation (if available></para>
+        /// <para>correlationId: The <see cref="string" /> containing the correlation id for the cmdlet (if available)</para>
+        /// <para>processRecordId: The <see cref="string" /> containing the correlation id for the individual process record. (if available)</para>
+        /// <para>exception: The <see cref="System.Exception" /> that is being thrown (if available)</para>
+        /// </example>
         public EventListenerDelegate EventListener;
 
         public EventListenerWithoutTelemetryDelegate EventListenerWithoutTelemetry;
@@ -69,27 +82,26 @@ namespace Microsoft.Azure.Commands.Common
         /// <summary>
         /// Called when the module is loading. Allows adding HTTP pipeline steps that will always be present.
         /// </summary>
-        /// <param name="resourceId"><c>string</c>containing the expected resource id (ie, ARM).</param>
-        /// <param name="moduleName"><c>string</c>containing the name of the module being loaded.</param>
-        /// <param name="prependStep">a delegate which allows the module to prepend a step in the HTTP Pipeline</param>
-        /// <param name="appendStep">a delegate which allows the module to append a step in the HTTP Pipeline</param>
+        /// <example>
+        /// <para>resourceId: <c>string</c>containing the expected resource id (ie, ARM).</para>
+        /// <para>moduleName: <c>string</c>containing the name of the module being loaded.</para>
+        /// <para>prependStep: a delegate which allows the module to prepend a step in the HTTP Pipeline</para>
+        /// <para>appendStep: a delegate which allows the module to append a step in the HTTP Pipeline</para>
+        ///</example>
         public ModuleLoadPipelineDelegate OnModuleLoad;
 
         /// <summary>
         /// Called when the cmdlet is constructing a new Request 
         /// </summary>
-        /// <param name="invocationInfo">The <see cref="System.Management.Automation.InvocationInfo" /> from the cmdlet</param>
-        /// <param name="correlationId">The <see cref="string" /> containing the correlation id for the cmdlet (if available)</param>
-        /// <param name="processRecordId">The <see cref="string" /> containing the correlation id for the individual process record. (if available)</param>
-        /// <param name="prependStep">a delegate which allows the module to prepend a step in the HTTP Pipeline</param>
-        /// <param name="appendStep">a delegate which allows the module to append a step in the HTTP Pipeline</param>
+        /// <example>
+        /// <para>invocationInfo: The <see cref="System.Management.Automation.InvocationInfo" /> from the cmdlet</para>
+        /// <para>correlationId: The <see cref="string" /> containing the correlation id for the cmdlet (if available)</para>
+        /// <para>processRecordId: The <see cref="string" /> containing the correlation id for the individual process record. (if available)</para>
+        /// <para>prependStep: a delegate which allows the module to prepend a step in the HTTP Pipeline</para>
+        /// <para>appendStep: a delegate which allows the module to append a step in the HTTP Pipeline</para>
+        /// </example>
         public NewRequestPipelineDelegate OnNewRequest;
 
-        /// <summary>
-        /// Called when the cmdlet is constructing a new Request by using proxy client
-        /// </summary>
-        /// <param name="prependStep">a delegate which allows the module to prepend a step in the HTTP Pipeline</param>
-        /// <param name="appendStep">a delegate which allows the module to append a step in the HTTP Pipeline</param>
         public NewProxyRequestPipelineDelegate OnNewProxyRequest;
 
         public NewRequestPipelineDelegate AddRequestUserAgentHandler;
@@ -99,13 +111,16 @@ namespace Microsoft.Azure.Commands.Common
         public AuthorizeRequestDelegate AddAuthorizeRequestHandler;
 
         /// <summary>
-        /// Called for well-known parameters that require argument completers
+        /// Called for well-known parameters that require argument completers, it
         /// </summary>
-        /// <param name="completerName">string - the type of completer requested (Resource, Location)</param>
-        /// <param name="invocationInfo">The <see cref="System.Management.Automation.InvocationInfo" /> from the cmdlet</param>
-        /// <param name="correlationId">The <see cref="string" /> containing the correlation id for the cmdlet (if available)</param>
-        /// <param name="resourceTypes">An <see cref="System.String[]"/> containing resource (or resource types) being completed  </param >
-        /// <param name="parentResourceParameterNames"> An <see cref="System.String[]"/> containing list of parent resource parameter names (if applicable)</param >
+        /// <example>
+        /// public string[] ArgumentCompleter(string completerName, System.Management.Automation.InvocationInfo invocationInfo, string correlationId, string[] resourceTypes, string[] parentResourceParameterNames)
+        ///     <para> completerName: string - the type of completer requested (Resource, Location)</para>
+        ///     <para> invocationInfo: The <see cref="System.Management.Automation.InvocationInfo" /> from the cmdlet</para>
+        ///     <para> correlationId: The <see cref="string" /> containing the correlation id for the cmdlet (if available)</para>
+        ///     <para> resourceTypes: An <see cref="System.String"/>[] containing resource (or resource types) being completed</para>
+        ///     <para> parentResourceParameterNames: An <see cref="System.String"/>[] containing list of parent resource parameter names (if applicable)</para>
+        /// </example>
         /// <returns>A <c>string[]</c> containing the valid options for the completer.</returns>
         public ArgumentCompleterDelegate ArgumentCompleter;
 
